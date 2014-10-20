037184c5d230dbd5ef12cddca65702a4d9f04020
Alexander Varchenko <alexander.varchenko@gmail.com>
2014-10-20 21:56:26 +0300

    fix example

---
+++ Example1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladm.DataModel;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new Ladm.DataModel.LadmDbContext())
+            {
+                /*Transaction transaction = new Transaction() { Id = 1, TransactionType = new TransactionMetaData() { Id = 1 } };
+                ctx.Transactions.Add(transaction);
+                ctx.SaveChanges();*/
+            }
+        }
+    }
+}
